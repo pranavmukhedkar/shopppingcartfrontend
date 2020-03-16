7f2ee08eb5d52e30827840628373f812d747e970
punegroup <32262037+punegroup@users.noreply.github.com>
2020-03-17 00:09:06 +0530

    first commit

---
+++ Scripts/GetProduct.js
@@ -0,0 +1,27 @@
+﻿function DisplayProduct(categoryId) {
+    debugger;
+    $.ajax({
+
+        type: "POST",
+        url: "/Home/GetProduct?id=" + categoryId,
+        contentType: "application/json; charset=utf-8",
+        datatype: "json",
+        success: function (data) {
+
+            $(".list-unstyled").find("li").remove();
+            //$("#ddlState").append("<option selected=selected value='0'>Select State</option>");
+            if (data != null)
+            {
+                $.each(data, function (index, value) {
+                    $(".list-unstyled").append('<li><a href="/Home/GetProduct?id=' + value.ProductId + '">' + value.ProductName + '</a><li>');
+                });
+            }
+            else
+            {
+                $(".list-unstyled").hide();
+            }
+        },
+
+    });
+
+}
